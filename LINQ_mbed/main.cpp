1da433d0029d2ceabb4e5efb33008967e348d1a2
jaga0228_mac <jaga0228@gmail.com>
2016-10-21 19:28:54 +0900

    send sensor values

--- LINQ_mbed/main.cpp
+++ LINQ_mbed/main.cpp
@@ -9,14 +9,14 @@
 mbed::Serial rs(PA_9, PA_10);
 mbed::DigitalOut rsSW(D3);
 
-float sendData[] = {11, 22, 33, 44, 55, 66, 77};
+float sendData[] = {1, 2, 3, 4, 5, 6, 7};
 
 //==============================================================
 void SerialAvailavle()
 {
 	int getData = rs.getc();
-	//	printf("%d\n", getData);
-	//	wait_ms(1);
+	//		printf("%d\n", getData);
+	wait_ms(1);
 	rsSW = 1;
 	if(getData < 7 && getData >= 0){
 		for (int i = 0; i < 7; i++) {
@@ -36,24 +36,28 @@ int main(int MBED_UNUSED argc, const char MBED_UNUSED * argv[])
 	//各種インスタンス
 	//  SRF05 usonic(D11, D12);
 	//  PwmOut pw    m(PB_0);
-//	PCA9547 mux(D4, D5, 0xE0);
+	PCA9547 mux(D4, D5, 0xE0);
+	mux.select(0);
 	
 	//温度センサのインスタンス作成
-//	mux.select(0);
-//	TPA81       temp2(D4, D5, 0xD0);
+	const int TEMP1 = 4;
+	mux.select(TEMP1);
+	TPA81       temp2(D4, D5, 0xD2);
 //	mux.select(1);
 //	TPA81   temp(D4, D5, 0xD2);
 	
+	const int IRDIST1 = 0;
+	const int IRDIST2 = 1;
+	//	IR距離センサのインスタンス作成
+	VL6180x irDist(D4, D5, 0x29 << 1);
+	irDist.VL6180xInit();
+	irDist.VL6180xDefautSettings();
+	for(int i = IRDIST1; i <= IRDIST2; i++) {
+		mux.select(i);
+		irDist.VL6180xInit();
+		irDist.VL6180xDefautSettings();
+	}
 	
-	//IR距離センサのインスタンス作成
-//	VL6180x irDist(D4, D5, 0x29 << 1);
-//	irDist.VL6180xInit();
-//	irDist.VL6180xDefautSettings();
-//	for(int i = 2; i < 6; i++) {
-//		mux.select(i);
-//		irDist.VL6180xInit();
-//		irDist.VL6180xDefautSettings();
-//	}
 	
 	//RS485初期設定
 	rs.baud(9600);
@@ -68,16 +72,28 @@ int main(int MBED_UNUSED argc, const char MBED_UNUSED * argv[])
 	mbed::DigitalOut ledDebug4(D12);
 	
 	while(1) {
-		//疑似センサ
-		sendData[0] += 0.01;
-		sendData[1] += 0.02;
-		sendData[2] += 0.03;
-		sendData[3] += 0.04;
-		sendData[4] += 0.05;
-		sendData[5] += 0.06;
-		sendData[6] += 0.07;
+
+		mux.select(IRDIST1);
+		sendData[0] = irDist.getDistance()/2;
+		mux.select(IRDIST2);
+		sendData[1] = irDist.getDistance()/2;
+		mux.select(TEMP1);
+		sendData[2] = temp2.getTemp(4);
 		for(int i = 0; i < 7; i++){
 			printf("%4d\t", (int)sendData[i]);
 		}puts("");
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
